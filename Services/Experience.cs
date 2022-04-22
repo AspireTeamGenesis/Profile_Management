4234879b7778fee8c2d88e0d933f519fbfae44c7
AspireTeamGenesis <103084831+AspireTeamGenesis@users.noreply.github.com>
2022-04-22 12:03:59 +0530

    services

---
+++ Services/Experience.cs
@@ -0,0 +1,9 @@
+class Experience
+{
+}
+class ExperienceFilter
+{
+    public  void Add(Experience experience){}
+    public  List<Experience> View(Experience experience){}
+    public  void Remove(int experience_id){}
+}
