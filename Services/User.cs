27e71c95d72ac6671fcc900a0888a8047ade7474
AspireTeamGenesis <103084831+AspireTeamGenesis@users.noreply.github.com>
2022-04-23 12:51:02 +0530

    Add files via upload

---
+++ Services/User.cs
@@ -0,0 +1,8 @@
+class User
+{
+    public bool Login(string Username,string Password){}
+    public bool forgotpassword(string email){}
+    public bool Changepassword(string Currentpassword,string Newpassword,string Confirmpassword){}
+    public bool Signout(){}
+
+}
