4150b20eb67701d81ecab723c667319c3127921b
AspireTeamGenesis <103084831+AspireTeamGenesis@users.noreply.github.com>
2022-04-25 12:13:43 +0530

    Add files via upload

---
+++ Services/Profile.cs
@@ -0,0 +1,73 @@
+
+class Profile
+{
+    PersonalDetails
+    List<EducationalDetails>
+    List<ProjectDetails>
+    List<Skills>
+    List<Achievements>
+}
+class PersonalDetails{
+List<Breakduration>
+List<Language>
+List<Socialmedia>
+}
+class EducationalDetails{
+
+}
+class ProjectDetails{
+
+}
+class Skills{
+
+}
+
+class Achievements{
+List<Awards>
+List<Certifications>
+}
+class Filters
+{
+   
+
+}
+
+class Profile_Operations{
+
+    public bool CreatePersonalDetails(PersonalDetails personaldetails){}
+    public bool UpdatePersonalDetails(PersonalDetails personaldetails){}
+    
+    public bool CreateEducationalDetails(List<EducationalDetails> educationaldetails){}
+    public List<EducationalDetails> ViewEducationalDetails(int profile_id){}
+    public bool UpdateEducationalDetails(List<EducationalDetails> educationaldetails){}
+    public bool DeleteEducationalDetails(int education_id){}
+    
+
+    public bool CreateProjectDetails(List<ProjectDetails> projects){}
+    public List<ProjectDetails> ViewProjectDetails(int profile_id){}
+    public bool UpdateProjectDetails(List<ProjectDetails> projectdetails){}
+    public bool DeleteProjectDetails(int project_Id){}
+   
+
+    public bool CreateSkills(List<Skills> skills){}
+    public List<Skills> ViewSkills(int profile_id){}
+    public bool UpdateSkills(List<Skills skills){}
+    public bool DeleteSkills(int skill_id){}
+      
+
+    public bool CreateAchievements(List<Achievements> achievements){}
+    public List<Achivements> ViewAchievements(int profile_id){}
+    public bool UpdateAchievements(List<Achievements> achievements){}
+
+
+
+    public List<Profile> search(string Name, List<Filters> filters){}
+    public bool Share(List<string> mailaddress){}
+    public bool Export(int profile_id){}
+    public bool RequesttoUpdate(string mailaddress){}
+    public List<Profile> ViewProfileHistory(int profile_id){}
+    
+
+}
+
+
