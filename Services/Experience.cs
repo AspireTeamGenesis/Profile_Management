c8ab8cb251322fb87f0d12e3d1e40203d409ccd5
AspireTeamGenesis <103084831+AspireTeamGenesis@users.noreply.github.com>
2022-04-25 12:15:58 +0530

    Delete Experience.cs

--- Services/Experience.cs
+++
@@ -1,9 +0,0 @@
-class Experience
-{
-}
-class ExperienceFilter
-{
-    public  void Add(Experience experience){}
-    public  List<Experience> View(Experience experience){}
-    public  void Remove(int experience_id){}
-}
