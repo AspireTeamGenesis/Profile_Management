5f58e9a77454d2a4987fbd50197484a2f140d186
AspireTeamGenesis <103084831+AspireTeamGenesis@users.noreply.github.com>
2022-04-26 11:32:28 +0530

    Delete User.cs

--- Services/User.cs
+++
@@ -1,8 +0,0 @@
-class User
-{
-    public bool Login(string Username,string Password){}
-    public bool forgotpassword(string email){}
-    public bool Changepassword(string Currentpassword,string Newpassword,string Confirmpassword){}
-    public bool Signout(){}
-
-}
