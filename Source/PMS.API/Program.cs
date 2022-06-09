ed312508f86c62cdc8ec9d97fdadc99b5033e0fb
mbrindha123 <100861206+mbrindha123@users.noreply.github.com>
2022-06-09 21:13:43 +0530

    Updated program.cs

--- Source/PMS.API/Program.cs
+++ Source/PMS.API/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.HttpLogging;
 using Microsoft.EntityFrameworkCore;
 using PMS_API;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,16 +15,34 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<Context>(options =>{
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddScoped<LoginService>();
+
 builder.Services.AddScoped<IUserServices,UserServices>();
+
 builder.Services.AddScoped<IPersonalService,PersonalService>();
 builder.Services.AddScoped<ICollegeServices,CollegeServices>();
 builder.Services.AddScoped<IDesignationServices,DesignationServices>();
 builder.Services.AddScoped<IDomainServices,DomainServices>();
 builder.Services.AddScoped<IProfileStatusServices,ProfileStatusServices>();
 builder.Services.AddScoped<ITechnologyServices,TechnologyServices>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata=false;
+    options.SaveToken=true;
+    options.TokenValidationParameters=new TokenValidationParameters()
+    {
+        ValidateIssuer=true,
+        ValidateAudience=true,
+        ValidAudience= builder.Configuration["Jwt:Audience"],
+        ValidIssuer=builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey=new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+}
+);
 builder.Services.AddControllersWithViews().AddNewtonsoftJson(options =>
 options.SerializerSettings.ReferenceLoopHandling=Newtonsoft.Json.ReferenceLoopHandling.Ignore
 );
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
