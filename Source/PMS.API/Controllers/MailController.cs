78545d38306f6d298145a5ec829ba641d630b7dd
mbrindha123 <100861206+mbrindha123@users.noreply.github.com>
2022-06-11 14:24:20 +0530

    commit

---
+++ Source/PMS.API/Controllers/MailController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using PMS_API;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MailController : ControllerBase
+{
+    private IMailService _mailService;
+    public MailController(IMailService mailService)
+    {
+        _mailService = mailService;
+    }
+    [HttpPost("send")]
+    public async Task<IActionResult> SendMail([FromForm]MailRequest request)
+    {
+        try
+        {
+            await _mailService.SendEmailAsync(request,true);
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+            
+    }
+}
