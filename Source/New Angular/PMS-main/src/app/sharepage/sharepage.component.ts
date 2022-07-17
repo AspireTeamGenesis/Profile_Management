c6b75129cdbc636133d8ca7d073ec51e2e3fc1ab
harinir01 <rharini426@gmail.com>
2022-07-17 14:55:15 +0530

    share updated

--- Source/New Angular/PMS-main/src/app/sharepage/sharepage.component.ts
+++ Source/New Angular/PMS-main/src/app/sharepage/sharepage.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Toaster } from 'ngx-toast-notifications';
 @Component({
   selector: 'app-sharepage',
   templateUrl: './sharepage.component.html',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SharepageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private toaster: Toaster) { }
 
   ngOnInit(): void {
   }
+  share(){
+    this.toaster.open({ text: 'Profile has been shared successfully via mail', position: 'top-center', type: 'success' })
 
+}
 }
