f86cd4bbe8196138c7d90ebe6a0a701963c3f54d
chitrarasu1601 <100710041+chitrarasu1601@users.noreply.github.com>
2022-07-21 14:35:49 +0530

    sidebar updates

--- Source/New Angular/PMS-main/src/app/sidebar/sidebar.component.ts
+++ Source/New Angular/PMS-main/src/app/sidebar/sidebar.component.ts
@@ -1,26 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
 import { AuthenticationService } from '../service/authentication.service';
 import { UserserviceService } from '../service/userservice.service';
 import { Router } from '@angular/router';
 import { animate, animateChild, query, style, transition, trigger } from '@angular/animations';
-
-
-
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
   animations: [
-
     trigger('ngIfAnimation', [
       transition(':enter, :leave', [
         query('@*', animateChild())
       ])
-
     ]),
     trigger('easeInOut', [
-
       transition('void => *', [
         style({
           opacity: 0
@@ -36,7 +29,6 @@ import { animate, animateChild, query, style, transition, trigger } from '@angul
         animate("500ms ease-in", style({
           opacity: 0
         }))
-
       ])
     ])
   ]
@@ -44,49 +36,48 @@ import { animate, animateChild, query, style, transition, trigger } from '@angul
 
 export class SidebarComponent implements OnInit {
 
-  profileId: any;
-  userId: number;
-  profileDetails: any;
+  profileId: number;
+  profileDetails = {
+    username: ''
+  };
   profileIdDetails: any;
-  constructor(private service: UserserviceService, private route: ActivatedRoute, private servicer: AuthenticationService, private router: Router) { }
+  isShowDivIf = false;
+
+  constructor(private service: UserserviceService, private servicer: AuthenticationService, private router: Router) { }
+
   ngOnInit(): void {
     this.getUserProfile();
-    // this.getProfileIdByUserId();
   }
+
   getUserProfile() {
-    console.warn("1")
     this.service.getUserProfile().subscribe({
       next: (data) => {
-        this.profileDetails = data,
-          console.log(this.profileDetails)
-      }
-
-    })
-  }
-  getProfileIdByUserId() {
-    console.warn("2")
-    this.service.getProfileIdByUserId().subscribe((data) => {
-      this.profileIdDetails = data;
-      this.profileId = this.profileIdDetails.profileId;
-      console.log('professor ' + this.profileId);
-      if (this.profileId == 0) {
-        this.router.navigateByUrl("/createprofile");
-      }
-      else {
-        this.router.navigateByUrl("/viewprofile");
+        this.profileDetails = data
       }
     })
-
   }
 
-
+  getProfileIdByUserId()
+  {
+    this.service.getProfileIdByUserId().subscribe({
+      next:(data)=> {
+        this.profileIdDetails=data,
+        this.profileId = this.profileIdDetails.profileId
+        if (this.profileId == 0) {
+          this.router.navigateByUrl("/createprofile");
+        }
+        else {
+          this.router.navigateByUrl("/viewprofile");
+        }
+      }
+    })
+  }
 
   logout() {
     this.servicer.ClearToken();
   }
-  isShowDivIf = false;
+
   toggleSidebar() {
     this.isShowDivIf = !this.isShowDivIf;
   }
-
 }
